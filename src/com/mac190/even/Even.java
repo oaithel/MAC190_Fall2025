6465322323685e4cd1fddfc96d4e9a3f5d8c5810
momo <oaithel@yahoo.com>
2025-09-30 14:00:25 -0400

    kjh

--- src/com/mac190/even/Even.java
+++ src/com/mac190/even/Even.java
@@ -22,6 +22,58 @@ public String toString() {
     //return the object.
 }
 In a seprate tester class, test your Even class.
+
+Constructors
+Overloading
+The concept of "this"
  */
 public class Even {
+    private int even;
+
+    //A constructor is amethod without a type with the same name as the class.
+    //it allows you to construct objects with initial attributes.
+    //The constructor that does not accept inputs is the default constructor
+
+    //default constructor
+    public Even(){
+        even = 2;
+    }
+    //Constructor that accepts a value
+    public Even(int num){
+        this.setEven(num);
+    }
+
+    //setter getter
+    public int getEven(){
+        return even;
+    }
+    public void setEven(int a){
+        if(a % 2 == 1){
+            a++;
+        }
+        even = a;
+    }
+
+    public String toString(){
+        return "even: " + even;
+    }
+
+    //add method to add a number to an existing Even object and return the new object
+    public Even add(int a){
+        //create Even object
+        Even E = new Even(this.even + a);
+        return E;
+    }
+    public Even add(Even a){
+        //How many Even object are there here? three: a, this and E the object we return
+        //create Even object
+        Even E = new Even(this.even + a.getEven());
+        return E;
+    }
+
+    //The sme method name used with different paremeters is called overloading.
+    //You can overload as long as the input parameters are of different type or different
+    //number. You cannot overload solely based on the return type.
+    //Example  public Even add(int a) and public int add(int a) is not allowed
+    //You cannot overload based ONLY on the return value
 }
--- src/com/mac190/even/EvenTester.java
+++ src/com/mac190/even/EvenTester.java
@@ -3,13 +3,30 @@ package com.mac190.even;
 public class EvenTester {
     public static void main(String[] args) {
         //create an Even object E1.
+        Even E1 = new Even();
         //display it using the toString
-
+        System.out.println("E1: " + E1);
         //set it to 9
+        E1.setEven(9);
         //display it.
-
-        //create a new Object that is E1 added to it 5, call it E2.
+        System.out.println("E1: " + E1);
+        //Create an Even object with value 11 right when you create it.
+        Even E2 = new Even(11);
+        System.out.println("E2: " + E2);
+        //create a new Object that is E1 added to it 5, call it E3.
+        Even E3 = E1.add(5); //this is the object on which a method is invoked. In this case
+        //add method is invoked on E1
         //display it.
+        System.out.println("E1: " + E1 + " E3: " + E3);
+        E2.add(9); //here this is E2
+        System.out.println("E2: " + E2); //E2 will be 12. add methods returned a new
+        //object with even = 22, but it's not used and destroyed
+        //To change E2 we do this
+        E2 = E2.add(9);
+        System.out.println("E2: " + E2);
+        //adding two Even objects
+        E3 = E1.add(E2);
+        System.out.println("E3: " + E3 + " E2: " + E2 + " E1: " + E1);
 
     }
 }
