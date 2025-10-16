6f7701beab05952c3862a592558c6f86a0702cba
ProfOmar <oaithela@yahoo.com>
2025-10-16 14:23:08 -0400

    10/16/2025

---
+++ src/com/mac190/maxminprime/MaxMinPrime.java
@@ -0,0 +1,11 @@
+package com.mac190.maxminprime;
+
+import com.mac190.prime.Prime;
+
+/*
+Modify the maxmin class we wrote so that the private members of the class
+are objects of the class Prime.
+ */
+public class MaxMinPrime {
+    private Prime min, max;
+}
---
+++ src/com/mac190/maxminprime/MaxMinPrimeTester.java
@@ -0,0 +1,33 @@
+package com.mac190.maxminprime;
+
+import com.mac190.maxmin.MaxMin;
+
+public class MaxMinPrimeTester {
+    //create a default MaxMin M1
+    MaxMinPrime M1 = new MaxMinPrime();
+        System.out.println("M1: " + M1);
+    //set min to 2 and max to 6
+        M1.setMaxMin(2, 6);
+        System.out.println("M1: " + M1);
+    //set min to 10
+        M1.setMin(10);
+        System.out.println("M1: " + M1);
+    MaxMinPrime M2 = new MaxMinPrime(5, 1);
+        System.out.println("M2: " + M2);
+    //Trace
+        M1.add(3);
+        System.out.println("M1: " + M1); // (6, 10) add does not change M1
+        M2.add(M1);
+        System.out.println("M2: " + M2); //(1, 5) add will not change M1 or M2
+    MaxMinPrime M3 = MaxMinPrime.add(M1, M2);
+        System.out.println("M3: " + M3); //(7, 15 we creating a new object
+    //add 3 to M1
+    M1 = M1.add(3);
+        System.out.println("M1: " + M1);
+    //Add M1 to M2
+    M2 = M2.add(M1);
+        System.out.println("M2: " + M2); //(10, 18)
+    //Same question using the static add method, add M1 to M2 using the static method
+    M2 = MaxMinPrime.add(M2, M1);
+        System.out.println("M2: " + M2);
+}
