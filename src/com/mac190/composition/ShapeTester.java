a74de8b55580caed233e06d88ef36d52dff85798
ProfOmar <oaithela@yahoo.com>
2025-10-16 13:35:08 -0400

    10/16/2025

--- src/com/mac190/composition/ShapeTester.java
+++ src/com/mac190/composition/ShapeTester.java
@@ -4,6 +4,31 @@ Test both classes in this tester.
  */
 public class ShapeTester {
     static void main() {
+        //create a default Rectangle
+        Rectangle R1 = new Rectangle();
+        //show it
+        System.out.println("R1: " + R1);
+        //set length to 4 width to 2
+        R1.setWidth(2);
+        R1.setLength(4);
+        System.out.println("R1: " + R1 + " Perim: " + R1.perimeter() + " area: " + R1.area());
+        //create ThreeDForm all sides same 5
+        ThreeDForm T1 = new ThreeDForm(5);
+        System.out.println("T1: " + T1);
+        //display the perim nd area of the base of T1
+        System.out.println("T1 base perim: " + T1.getBase().perimeter() + " T1 base area: " + T1.getBase().area());
+
+        //add 5 to R1
+
+        //create R2 a Rectangle with 3 and 4, display it
+
+        //create R3 that is the sum of R1 and R2, display it
+
+        //Create T2 a ThreeDForm with R2 and 4, display it
+
+        //add T2 to T1, display both
+
+        //create R4 that is the sum of the bases of T1 and T2, display it.
 
     }
 }
--- src/com/mac190/composition/ThreeDForm.java
+++ src/com/mac190/composition/ThreeDForm.java
@@ -1,4 +1,7 @@
 package com.mac190.composition;
+
+import java.util.Objects;
+
 /*
 design a class ThreeDForm that has the following properties:
 1- a private member base which is a Rectangle object as the base
@@ -19,9 +22,85 @@ Rectangle getBase()
 all sides augmented by a
 10- static ThreeDForm add(ThreeDForm t1, ThreeDForm t2) returns a ThreeDForm
 that is the sum of t1 and t2.
+11- method volume, which is the area of the base multiplied by the height
 11- In a tester ShapeTester, test both classes extensively.
  */
 public class ThreeDForm {
     private Rectangle base;
     private double height;
+
+    //constructors
+    public ThreeDForm(){
+        base = new Rectangle();
+        height = 0;
+    }
+    public ThreeDForm(double a){
+        base = new Rectangle(a);
+        height = Math.abs(a);
+    }
+
+    public ThreeDForm(Rectangle r, double a){
+        base = new Rectangle(r.getLength(), r.getWidth()); //this is a preference
+        //so that you have an object independent of r.
+        height = Math.abs(a);
+    }
+
+    public Rectangle getBase() {
+        return base;
+    }
+    public void setBase(Rectangle base) {
+        this.base = base;
+    }
+    public double getHeight() {
+        return height;
+    }
+    public void setHeight(double height) {
+        this.height = Math.abs(height);
+    }
+    public double getLength(){
+        return base.getLength();
+    }
+    public void setLength(double l){
+        base.setLength(l);
+    }
+    public double getWidth(){
+        return base.getWidth();
+    }
+    public void setWidth(double l){
+        base.setWidth(l);
+    }
+
+    @Override
+    public String toString() {
+        return "ThreeDForm{" +
+                "base=" + base +
+                ", height=" + height +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        ThreeDForm that = (ThreeDForm) o;
+        return Double.compare(height, that.height) == 0
+                && this.base.equals(that.getBase());
+    }
+    //ThreeDForm add (int a) returns a threeDForm same as this with
+    //all sides augmented by a
+
+    public ThreeDForm add(int a){
+        ThreeDForm T = new ThreeDForm(Rectangle.add(this.base, a), this.height+a);
+        return T;
+    }
+
+    //10- static ThreeDForm add(ThreeDForm t1, ThreeDForm t2) returns a ThreeDForm
+    //that is the sum of t1 and t2.
+    public static ThreeDForm add(ThreeDForm T1, ThreeDForm T2){
+        ThreeDForm T3 = new ThreeDForm(T1.base.add(T2.base), T1.height + T2.height);
+        return T3;
+    }
+    public double volume(){
+        return this.base.area()*this.height;
+    }
+
 }
