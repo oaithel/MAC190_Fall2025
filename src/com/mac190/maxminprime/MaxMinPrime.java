50716c758763ec2796d8bc59e0d693cd5f051f47
ProfOmar <oaithela@yahoo.com>
2025-10-16 14:37:59 -0400

    10/16/2025

--- src/com/mac190/maxminprime/MaxMinPrime.java
+++ src/com/mac190/maxminprime/MaxMinPrime.java
@@ -1,5 +1,6 @@
 package com.mac190.maxminprime;
 
+import com.mac190.maxmin.MaxMin;
 import com.mac190.prime.Prime;
 
 /*
@@ -8,4 +9,53 @@ are objects of the class Prime.
  */
 public class MaxMinPrime {
     private Prime min, max;
-}
+
+    //default constructor
+    //constructor that accepts one integer
+    //constructor that accepts two integers
+    //constructor that accepts one Prime
+    //construcotr that accepts two primes
+
+    public int getMin(){
+
+    }
+    public int getMax(){
+
+    }
+    public Prime getMinPrime(){
+
+    }
+    public Prime getMaxPrime(){
+
+    }
+    public void setMin(int a){
+
+    }
+    public void setMin(Prime a){
+
+    }
+    public void setMax(int a){
+
+    }
+    public void setMax(Prime a){
+
+    }
+    public void setMaxMin(int a, int b){
+
+    }
+    public void setMaxMin(Prime a, Prime b){
+
+    }
+
+    //toString()
+
+    //equals()
+
+    public MaxMinPrime add(int a){
+    }
+    public MaxMinPrime add(Prime m){
+    }
+    public MaxMinPrime add(MaxMinPrime m){
+    }
+    public static MaxMinPrime add(MaxMinPrime m1, MaxMinPrime m2){
+    }
