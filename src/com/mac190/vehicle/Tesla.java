631efd6def1f8215964fbd66952aac2e4e361e78
ProfOmar <oaithel@yahoo.com>
2025-11-13 13:46:59 -0500

    11/13/2025

--- src/com/mac190/vehicle/Tesla.java
+++ src/com/mac190/vehicle/Tesla.java
@@ -1,9 +1,10 @@
 package com.mac190.vehicle;
+
 /*
-Design a class BMW, that is a Vehicle, in adition it has the following:
-FuelType is gas
-Make is "BMW"
-Model (example "X6")
+Design a class Tesla, that is a Vehicle, in addition it has the following:
+FuelType is electric
+Make is "Tesla"
+Model of the car
 It has a Color and number of seats as well.
 
 Implement the methods as follows:
@@ -13,12 +14,13 @@ Brake implement it so that the car is in stop mode
 moveForward implement it so that the car moves forward only if it was in state
 stopping state already
 Same for moveBackwards
-implement speedUp to increase the speed by the input
-and display the speed, limit the speed to 100miles/hour
-speedUp only increases the speed if it is movingForward.
+implement speedUp to increase the speed by no more than 10miles/hour
+or the input and display the speed, limit the speed to 80miles/hour
+speedUp only increases if it is movingForward.
 
 Test your class.
 
  */
-public class BMW extends Vehicle{
+public class Tesla extends Vehicle{
+
 }
