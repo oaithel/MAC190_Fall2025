e7784388309037e8669ae64f8aa354fa120538f1
momo <oaithel@yahoo.com>
2025-12-02 13:57:30 -0500

    12/2/2025

---
+++ src/com/mac190/files/filesExample.java
@@ -0,0 +1,21 @@
+package com.mac190.files;
+/*
+Write a program that accepts the absolute path of a file.
+Create the file and ask the user to input lines of text and the end of the text
+will be signaled by a . on an empty line.
+Example:
+Enteer the file name: C:\\Users\\momo\\LAGCC\\courses\\Mac190\\test.txt
+Enter you text, end it with a dot on an empty line:
+Hello there,
+This is my text patati patata
+dfjhdf df;lkdfh
+sdfk;ooso'  and so on
+ .
+ You text has been saved to the file.
+ (If you check the file you should see the ext without the dot at the end.
+ */
+public class filesExample {
+    public static void main(String[] args) {
+
+    }
+}
